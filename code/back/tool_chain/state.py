7d2d73c4be1408dd45cd9e9f527d2dc90e6b37fe
郭岩 <10655176+guo-yan123@user.noreply.gitee.com>
2026-01-13 16:00:27 +0800

    state修改

--- code/back/tool_chain/state.py
+++ code/back/tool_chain/state.py
@@ -3,6 +3,15 @@ from typing_extensions import TypedDict
 
 
 class State(TypedDict):
-    text: str
-    data: List[List[Dict]]
-    response: str
+    
+    data: List[List[Dict]]  #输入数据
+    text: str #处理后的数据
+    new_feature: str #新特征
+    new_rule: str #新规则
+    feature: str #检索出的相关特征特征
+    feature_matching: str #特征匹配结果
+    rule : str #检索出的相关规则
+    rule_matching : str #规则匹配结果
+    report: str #风险报告
+    risk: str #风险评分
+    response: str #回答
